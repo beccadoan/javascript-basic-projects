bf34e7c57412a4546d54f32d26ef45f5d848ab50
Becca Doan <rebecca.doan@yahoo.com>
2021-09-01 18:14:10 -0400

    finished color flipper project

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,4 +1,17 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 
 const btn = document.getElementById('btn');
-const color = document.querySelector('.color')
+const color = document.querySelector('.color');
+
+btn.addEventListener('click',function (){
+    //get random number between 0 and 3
+    
+    const randomNumber = getRandomNumber(0,3);
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,27 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click',function (){
+    //get random number between 0 and 3
+    
+    const hexValue = generateHexCode();
+    console.log(hexValue);
+    document.body.style.backgroundColor = hexValue;
+    color.textContent = hexValue;
+})
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+function generateHexCode(){
+    let hexArray = ['#'];
+    for(let i=0; i<6; i++){
+        const random = getRandomNumber(0,15);
+        hexArray.push(hex[random]);
+    }
+    return hexArray.join('');
+}
