4bb08d2922a82f0fa53b9ae0e9d97b4769f0ff97
Becca Doan <rebecca.doan@yahoo.com>
2021-09-02 17:08:14 -0400

    finished coding reviews project

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -37,3 +37,51 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+//select items 
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting items
+let currentItem = 0;
+
+// load initial item
+window.addEventListener('DOMContentLoaded', updateReview());
+
+function updateReview(){
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+prevBtn.addEventListener('click', function(){
+  if(currentItem == 0){
+    currentItem = reviews.length - 1;
+  } else{
+    currentItem--;
+  }
+ updateReview();
+});
+
+nextBtn.addEventListener('click', function(){
+  if(currentItem == reviews.length - 1){
+    currentItem = 0;
+  } else{
+    currentItem++;
+  }
+  updateReview();
+});
+
+randomBtn.addEventListener('click', function(){
+  currentItem = Math.floor(Math.random()*4);
+  console.log(currentItem);
+  updateReview();
+});
